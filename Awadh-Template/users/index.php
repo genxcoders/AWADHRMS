4ad35c0de80e9d49c580296768ccdff2c21833f8
genxcoders <genxcoders@gmail.com>
2019-10-05 17:42:43 +0530

    05102019 Aashish 1742: User Profile edit page and HTML segregation

---
+++ Awadh-Template/users/index.php
@@ -0,0 +1,10 @@
+<?php require_once( '../couch/cms.php' ); ?>
+
+<cms:template clonable='1' title='Users' hidden='1'>
+    <!-- 
+        If additional fields are required for users, they can be defined here in the usual manner.
+    -->        
+
+</cms:template>
+
+<?php COUCH::invoke(); ?>
