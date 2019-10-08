f5c234d391cf174463771db88f95527cff90b551
genxcoders <genxcoders@gmail.com>
2019-10-08 20:29:00 +0530

    08102019 Aashish 2028: Day end Commit

--- test/test-modal.php
+++ test/test-modal.php
@@ -8,55 +8,43 @@
 </head>
 <body>
 	<cms:pages masterpage="department/department.php">
-	<button type="button" class="btn btn-primary"  data-toggle="modal" data-target="#exampleModal" data-whatever="<cms:show k_page_id />">Open <cms:show k_page_title /> [<cms:show k_page_id />]</button><br>
+	<button type="button" class="btn btn-success openBtn" id="<cms:show k_page_id />/delete"><cms:show k_page_title /></button>
 	</cms:pages>
 
 
 
-	<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-					<h4 class="modal-title" id="exampleModalLabel">New message</h4>
-				</div>
-				<div class="modal-body">
-					<cms:set my_page_id="<span></span>" scope="global" />
-					ASD::<cms:show my_page_id />
-					<cms:form 
-                            masterpage='department/department.php'
-                            mode='edit'
-                            page_id='<cms:show my_page_id />'
-                            method='post' 
-                            anchor='0'
-                            id='delete_department_form'
-                            name='delete_department_form'
-                        >
-                        <cms:show k_page_id />
-                    </cms:form>
-					
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-					<button type="button" class="btn btn-primary">Send message</button>
-				</div>
-			</div>
-		</div>
-	</div>
+	<div class="modal fade" id="myModal" role="dialog">
+        <div class="modal-dialog">
+        
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Bootstrap Modal with Dynamic Content</h4>
+                </div>
+                <div class="modal-body">
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+          
+        </div>
+    </div>
 
 	<script type="text/javascript" src="../assets/js/jquery-2.0.0.js"></script>
 	<script type="text/javascript" src="../assets/js/bootstrap.js"></script>
 	<script type="text/javascript">
-		$('#exampleModal').on('show.bs.modal', function (event) {
-			var button = $(event.relatedTarget) // Button that triggered the modal
-			var recipient = button.data('whatever') // Extract info from data-* attributes
-			// If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-			// Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-			var modal = $(this)
-			modal.find('.modal-title').text('New message to ' + recipient)
-			modal.find('.modal-body input').val(recipient)
-			modal.find('span').text(recipient);
-		})
+		$('.openBtn').on('click',function(){
+		    /*$('.modal-body').load('content.html',function(){
+		        $('#myModal').modal({show:true});
+		    })*/
+			var id = $(this).attr('id');
+		    $('.modal-body').load('<cms:embed "department/<cms:show k_matched_route />.html" />'+id,function(){
+		        $('#myModal').modal({show:true});
+		    });
+		});
 	</script>
 
 </body>
