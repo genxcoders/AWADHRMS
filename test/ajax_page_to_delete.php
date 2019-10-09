12ede601f7fab3ec200a5bed64c429442d299158
genxcoders <genxcoders@gmail.com>
2019-10-10 02:48:24 +0530

    10102019 Aashish rgba(0,0,0,0.08) 0248: Night Work

--- test/ajax_page_to_delete.php
+++ test/ajax_page_to_delete.php
@@ -2,21 +2,9 @@
 <cms:template title='Modal - Delete Id' parent='_test_' />
 <cms:set my_delete_page_id="<cms:gpc 'id' method='get' />" scope="global" />
 <cms:set mytemplate="<cms:gpc 'mytemplate' method='get' />" scope="global" />
-<cms:db_persist
-    _auto_title             =   '0'
-    _invalidate_cache       =   '0'
-    _masterpage             =   '<cms:show mytemplate />'
-    _mode                   =   'edit'
-    _page_id				=	'<cms:show my_delete_page_id />'
->
-	<cms:if k_error >
-        <cms:abort>
-            <cms:each k_error >
-                <br><cms:show item />
-            </cms:each>
-        </cms:abort>
-    <cms:else_if k_success />
+
+    <cms:if mytemplate && my_delete_page_id >
+        <cms:db_delete masterpage=mytemplate page_id=my_delete_page_id />
         <cms:abort>Page Deleted</cms:abort>
     </cms:if>
-</cms:db_persist>
 <?php COUCH::invoke( ); ?>
--- test/test-modal.php
+++ test/test-modal.php
@@ -5,14 +5,81 @@
 <head>
 	<title></title>
 	<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">	
+	<link rel="stylesheet" type="text/css" href="../assets/css/gxcpl.css">	
+	<link rel="stylesheet" type="text/css" href="../assets/css/font-awesome.css">	
 </head>
 <body>
-	<cms:pages masterpage="department/department.php">
-	<button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal" data-whatever="<cms:show k_page_id />">
-		<cms:show k_page_title /> <small><em>{<cms:show k_page_id />}</em></small>
-	</button>
-	<div style="padding-top: 10px;"></div>
-	</cms:pages>
+	<table class="gxcpl-card-shadow gxcpl-card-white">
+		<thead class="gxcpl-table-header">
+			<tr>
+				<th width="65px">
+					Sr. No.
+				</th>
+				<th width="*">
+					Department Name
+				</th>
+				<th width="30%">
+					Action
+				</th>
+			</tr>
+		</thead>
+
+		<tbody class="gxcpl-table-body">
+			<cms:pages masterpage='department/department.php' paginate='1' limit='10' show_future_entries='1' >
+			<cms:no_results>
+			<tr>
+				<td colspan="3" class="text-center">
+					- No Departments Found -
+				</td>
+			</tr>
+			</cms:no_results>
+			<tr>
+				<td class="text-center">
+					<cms:show k_absolute_count />.
+				</td>
+				<td>
+					<cms:show k_page_title /> [<cms:show k_page_id />]
+				</td>
+				<td>
+					<div class="btn-group btn-group-xs" role="group" aria-label="...">
+						<button type="button" class="btn btn-warning gxcpl-btn gxcpl-fc-21" onclick="window.location='<cms:route_link "edit_department" rt_id=k_page_id />';">
+							<i class="fa fa-edit"></i> EDIT
+						</button>
+
+						
+						<button type="button" class="btn btn-danger gxcpl-btn" data-toggle="modal" data-target="#myModal" data-whatever="<cms:show k_page_id />">
+							<i class="fa fa-trash"></i> Delete
+						</button>
+						
+					</div>
+				</td>
+			</tr>
+			</cms:pages>
+		</tbody>
+		<tfoot class="gxcpl-table-footer">
+			<tr>
+				<td colspan="3" class="text-center">
+					<cms:pages masterpage=k_template_name show_future_entries="1" paginate='1' limit='10'>
+					<cms:paginator />
+					</cms:pages>
+					<!-- <div class="btn-group btn-group-xs" role="group" aria-label="...">
+						<button type="button" class="btn btn-default">
+							<
+						</button>
+						<button type="button" class="btn btn-default">
+							1
+						</button>
+						<button type="button" class="btn btn-default">
+							2
+						</button>
+						<button type="button" class="btn btn-default">
+							>
+						</button>
+					</div> -->
+				</td>
+			</tr>
+		</tfoot>
+	</table>
 
 
 	<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
@@ -23,7 +90,8 @@
 					<h4 class="modal-title" id="exampleModalLabel">New message</h4>
 				</div>
 				<div class="modal-body">
-					<cms:pages masterpage='department/department.php' page_id=k_page_id limit='1' show_future_entries='1' >
+					<cms:ignore>
+					<!-- <cms:pages masterpage='department/department.php' page_id=k_page_id limit='1' show_future_entries='1' >
 
 					   	<cms:form
 							masterpage='department/department.php'
@@ -35,7 +103,6 @@
 							<cms:show k_page_id />::<cms:show k_template_name />
 							<br>
 							<cms:if k_success>
-								<!-- Delete the specified page -->
 								<cms:db_delete masterpage=k_template_name page_id=k_page_id /> 
 								<cms:redirect k_template_link />     
 							<cms:else_if k_error />
@@ -47,7 +114,8 @@
 							<input type="submit" name="submit" value="Delete page"/>
 						</cms:form>
 
-					</cms:pages>
+					</cms:pages> -->
+					</cms:ignore>
 				</div>
 				<div class="modal-footer">
 					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -64,16 +132,16 @@
 			var recipient = button.data('whatever') // Extract info from data-* attributes
 			// If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
 			// $(document).on('click','#delete_page',function(e) {
-				// event.preventDefault();
-				// $.ajax({
-				// 	url:"ajax_page_to_delete.php",
-				// 	data:{mytemplate:'<cms:show k_template_name />', id:button.data('whatever')},
-				// 	contentType:"application/json; charset=utf-8",
-				// 	success: function(response)
-				// 	{
-				// 		console.log(response);
-				// 	}
-				// });
+				event.preventDefault();
+				$.ajax({
+					url:"ajax_page_to_delete.php",
+					data:{mytemplate:'department/department.php', id:button.data('whatever')},
+					contentType:"application/json; charset=utf-8",
+					success: function(response)
+					{
+						console.log(response);
+					}
+				});
 			// });
 			// Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
 			// var modal = $(this)
